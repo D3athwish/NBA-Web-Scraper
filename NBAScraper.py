7852f276216b556f4ae6e00ee0b12c608fa0139c
D3athwish <61936623+D3athwish@users.noreply.github.com>
2020-10-28 19:30:34 +0100

    Update: FINAL version
    Update: FINAL version

--- NBAScraper.py
+++ NBAScraper.py
@@ -1,5 +1,5 @@
-import time
 from selenium import webdriver
+import time
 
 
 def x_path(string):
@@ -13,15 +13,10 @@ def element_id(string):
     return ""
 
 
-def element_class(string):
+def element_class(string, user):
     time.sleep(10)
-    return driver.find_element_by_class_name(string).click()
-
-
-def get_row_number():
-    return len(driver.find_elements_by_css_selector("nba-stat-table.stats-table-next:nth-child(8) > div:nth-child(2) "
-                                                    "> div:nth-child(1) > table:nth-child(1) > "
-                                                    "tbody:nth-child(2) > tr")) + 1
+    driver.find_element_by_class_name(string).send_keys(user)
+    return 0
 
 
 # User inputs player name
@@ -37,7 +32,7 @@ driver.get("https://www.nba.com/players")
 element_id("onetrust-accept-btn-handler")
 
 # Click searchbox to start searching, and input player name into searchbox
-element_class("border").send_keys(userInput)
+element_class("border", userInput)
 
 # Click first result
 x_path("/html/body/div[1]/div[2]/div[3]/section/div/div[2]/div["
@@ -46,13 +41,20 @@ x_path("/html/body/div[1]/div[2]/div[3]/section/div/div[2]/div["
 # Click stats button to navigate to NBA players stats
 x_path("/html/body/div[1]/div[2]/div[3]/div/div[1]/div/ul/li[2]/a").click()
 
-# Get number of rows and loop over them
-for _ in range(1, get_row_number()):
-    elementDate = x_path(
-        "/html/body/main/div/div/div/div[4]/div/div/div/div/nba-stat-table[1]/div[2]/div[1]/table/tbody/tr[" + str(_) +
-        "]/td[1]")
+time.sleep(10)
 
-    elementPA = x_path(
+# Get number of rows
+row_number_string = "nba-stat-table.stats-table-next:nth-child(8) > div:nth-child(2)> div:nth-child(1) > " \
+                    "table:nth-child(1) > tbody:nth-child(2) > tr"
+
+row_number = len(driver.find_elements_by_css_selector(row_number_string)) + 1
+
+# Loop over rows
+for _ in range(1, row_number):
+    elementDate = driver.find_element_by_xpath("/html/body/main/div/div/div/div[4]/div/div/div/div/nba-stat-table["
+                                               "1]/div[2]/div[1]/table/tbody/tr[" + str(_) + "]/td[1]")
+
+    elementPA = driver.find_element_by_xpath(
         "/html/body/main/div/div/div/div[4]/div/div/div/div/nba-stat-table[1]/div[2]/div[1]/table/tbody/tr[" + str(_) +
         "]/td[10]")
 
